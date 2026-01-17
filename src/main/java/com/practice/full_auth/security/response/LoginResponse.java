4f7ba1ec8ce03bd5d06e281fc835eeb2575625a3
DESKTOP-LA3KBVD\It <shuaib6950@gmail.com>
2026-01-17 12:52:44 +0600

    Implement JWT process with basic auth

---
+++ src/main/java/com/practice/full_auth/security/response/LoginResponse.java
@@ -0,0 +1,21 @@
+package com.practice.full_auth.security.response;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponse {
+    private String jwtToken;
+    private String username;
+    private List<String> roles;
+
+    public LoginResponse(String username, List<String> roles, String jwtToken) {
+        this.username = username;
+        this.roles = roles;
+        this.jwtToken = jwtToken;
+    }
+}
+
