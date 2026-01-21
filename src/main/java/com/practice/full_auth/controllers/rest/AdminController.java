24bc983c800156163065afadb30db4c92d20fab5
Shuaib <shuaib6950@gmail.com>
2026-01-21 22:19:14 +0600

    implement user manupulation endpoints

--- src/main/java/com/practice/full_auth/controllers/rest/AdminController.java
+++ src/main/java/com/practice/full_auth/controllers/rest/AdminController.java
@@ -1,5 +1,7 @@
 package com.practice.full_auth.controllers.rest;
 
+import com.practice.full_auth.models.Role;
+import com.practice.full_auth.services.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,5 +40,36 @@ public class AdminController {
         return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
     }
 
+    @PutMapping("/update-lock-status")
+    public ResponseEntity<String> updateAccountLockStatus(@RequestParam Long userId, @RequestParam boolean lock) {
+        userService.updateAccountLockStatus(userId, lock);
+        return ResponseEntity.ok("Account lock status updated");
+    }
 
+    @GetMapping("/roles")
+    public List<Role> getAllRoles() {
+        return userService.getAllRoles();
+    }
+
+    @PutMapping("/update-expiry-status")
+    public ResponseEntity<String> updateAccountExpiryStatus(@RequestParam Long userId, @RequestParam boolean expire) {
+        userService.updateAccountExpiryStatus(userId, expire);
+        return ResponseEntity.ok("Account expiry status updated");
+    }
+
+    @PutMapping("/update-enabled-status")
+    public ResponseEntity<String> updateAccountEnabledStatus(@RequestParam Long userId, @RequestParam boolean enabled) {
+        userService.updateAccountEnabledStatus(userId, enabled);
+        return ResponseEntity.ok("Account enabled status updated");
+    }
+
+    @PutMapping("/update-password")
+    public ResponseEntity<String> updatePassword(@RequestParam Long userId, @RequestParam String password) {
+        try {
+            userService.updatePassword(userId, password);
+            return ResponseEntity.ok("Password updated");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/practice/full_auth/services/UserService.java
+++ src/main/java/com/practice/full_auth/services/UserService.java
@@ -3,6 +3,7 @@ package com.practice.full_auth.services;
 import java.util.List;
 
 import com.practice.full_auth.dtos.UserDTO;
+import com.practice.full_auth.models.Role;
 import com.practice.full_auth.models.User;
 
 public interface UserService {
@@ -13,4 +14,14 @@ public interface UserService {
     UserDTO getUserById(Long id);
 
     User findByUsername(String username);
+
+    void updateAccountLockStatus(Long userId, boolean lock);
+
+    List<Role> getAllRoles();
+
+    void updateAccountExpiryStatus(Long userId, boolean expire);
+
+    void updateAccountEnabledStatus(Long userId, boolean enabled);
+
+    void updatePassword(Long userId, String password);
 }
